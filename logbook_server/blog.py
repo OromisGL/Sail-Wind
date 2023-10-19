705535eff11b09fb6bb4edb16b567eab5072bfda
Florian Lamprecht <git@wesx.de>
2023-10-19 20:33:28 +0200

    Rename flaskr application to logbook-server

--- logbook_server/blog.py
+++ logbook_server/blog.py
@@ -4,8 +4,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from logbook_server.auth import login_required
+from logbook_server.db import get_db
 
 bp = Blueprint('blog', __name__)
 
