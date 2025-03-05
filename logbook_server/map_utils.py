1eda5cc1d7d316c3dfa5aa7069fa39a781521f7f
louis <louis.kraemer@gfn.education>
2025-03-05 12:13:33 +0100

    a few functios to process wind data and fetch it from the Station in radius of 30 km.

--- logbook_server/map_utils.py
+++ logbook_server/map_utils.py
@@ -1,9 +1,20 @@
 from gpxplotter import read_gpx_file, create_folium_map, add_segment_to_map
 from gpxplotter.common import RELABEL
-from datetime import datetime
-import wetterdienst 
+from datetime import datetime  
+from wetterdienst import Settings
+from wetterdienst.provider.dwd.observation import DwdObservationRequest
+import polars as pl
 import folium
+from folium.plugins import FloatImage
+import base64
 import branca.colormap as cm
+
+
+settings = Settings(
+    ts_shape="long",         # Ausgabe als lange Tabelle
+    ts_humanize=True,        # Parameter verständlicher anzeigen
+    ts_convert_units=True    # Werte in SI-Einheiten umwandeln
+) 
 
 line_options = {'weight': 8}
 color_by = 'Velocity / km/h'
@@ -31,24 +42,119 @@ def build_map(path):
 #   print(track)
 #    print("end track")
 
+def station_request(lat, lon):
+    
+    location = (lat, lon)
+    
+    station = DwdObservationRequest(
+    parameters=("10_minutes", "wind"),
+    settings=settings
+    ).filter_by_distance(latlon=location, distance=30) # Station request with in a radius of 30 km
+    
+    df = station.df.head()
+    station = df[0]
+    station_id = station.select("station_id").item() # Station Id for next request, can not access the data in on request 
+    
+    return station_id
+
+def wind_velo(station):
+    request = DwdObservationRequest(
+        parameters=("10_minutes", "wind"),
+        settings=settings
+    ).filter_by_station_id(station_id=(station, )) # Get the Station Data
+    
+    values = request.values.all().df
+    values = values.with_columns(pl.col("date").cast(pl.Utf8))  
+    values = values.with_columns(pl.col("date").str.to_datetime())
+    
+    wind_speed = values.filter(pl.col("parameter") == "wind_speed").sort("date").tail(1).select("value").item()
+
+    return wind_speed # wind speed in m/s
+
+def wind_direct(station):
+    request = DwdObservationRequest(
+        parameters=("10_minutes", "wind"),
+        settings=settings
+    ).filter_by_station_id(station_id=(station, ))
+    
+    values = request.values.all().df
+    values = values.with_columns(pl.col("date").cast(pl.Utf8))  
+    values = values.with_columns(pl.col("date").str.to_datetime())
+    
+    wind_direction = values.filter(pl.col("parameter") == "wind_direction").sort("date").tail(1).select("value").item()
+
+    return wind_direction # direction in degree
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
+    
+def beafort(wind_speed):
+    if wind_speed <= 0.2:
+        return 0
+    if wind_speed <= 1.5:
+        return 1
+    if wind_speed <= 3.3:
+        return 2
+    if wind_speed <= 5.4:
+        return 3
+    if wind_speed <= 7.9:
+        return 4
+    if wind_speed <= 10.7:
+        return 5
+    if wind_speed <= 13.8:
+        return 6
+    if wind_speed <= 17.1:
+        return 7
+    if wind_speed <= 20.7:
+        return 8
+    if wind_speed <= 24.4:
+        return 9
+    if wind_speed <= 28.4:
+        return 10
+    if wind_speed <= 32.6:
+        return 11
+    
+    return 12
+    
+def wind_compass(wind_direction):
+    if wind_direction <= 10 or wind_direction in range(341, 361):
+        return 'N'
+    if wind_direction in range(11, 31):
+        return 'N/NO'
+    if wind_direction in range(31, 51):
+        return 'NO'
+    if wind_direction in range(51, 71):
+        return 'O/NO'
+    if wind_direction in range(71, 101):
+        return 'O'
+    if wind_direction in range(101, 121):
+        return 'O/SO'
+    if wind_direction in range(121, 141):
+        return 'SO'
+    if wind_direction in range(141, 161):
+        return 'S/SO'
+    if wind_direction in range(161, 191):
+        return 'S'
+    if wind_direction in range(191, 211):
+        return 'S/SW'
+    if wind_direction in range(211, 231):
+        return 'SW'
+    if wind_direction in range(231, 251):
+        return 'W/SW'
+    if wind_direction in range(251, 281):
+        return 'W'
+    if wind_direction in range(281, 301):
+        return 'W/NW'
+    if wind_direction in range(301, 321):
+        return 'NW'
+    if wind_direction in range(321, 341):
+        return 'N/NW'
+    
+
 def builld_default_map(latitude, longitude):
     the_map = create_folium_map(tiles='openstreetmap', max_bounds=True)
     folium.Marker([latitude, longitude], icon=folium.Icon(color='red')).add_to
-    the_map.fit_bounds([[latitude - 0.05, longitude - 0.05],
-                        [latitude + 0.05, longitude + 0.05]])
+    the_map.fit_bounds([[latitude - 0.035, longitude - 0.035],
+                        [latitude + 0.035, longitude + 0.035]])
     return the_map
-
-
-
-# Create client object.
-dwd = DwdWeather(resolution="hourly")
-
-# Find closest station to position.
-closest = dwd.nearest_station(lon=7.0, lat=51.0)
-
-# The hour you're interested in.
-# The example is 2014-03-22 12:00 (UTC).
-query_hour = datetime(2014, 3, 22, 12)
-
-result = dwd.query(station_id=closest["station_id"], timestamp=query_hour)
-print(result)
